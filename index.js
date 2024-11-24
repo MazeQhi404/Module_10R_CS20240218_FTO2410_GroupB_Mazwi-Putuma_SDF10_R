972c76a84a4ca94d7d94528ddab6bdb5ff417ee2
MazeQhi404 <maze.putuma@gmail.com>
2024-11-24 18:07:26 +0200

    firebase set-up practise

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-import { add } from "../functions.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 
-console.log(add(1,2))
+ import { getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://playground-ab742-default-rtdb.firebaseio.com/"
 }
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const moviesInDB = ref(database, "movies")
+console.log(app)
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
@@ -12,6 +17,8 @@ const addButtonEl = document.getElementById("add-button")
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
     
-    console.log(inputValue)
+    push(moviesInDB, inputValue)
+
+    console.log(`${inputValue} added to database`)
 })
 
